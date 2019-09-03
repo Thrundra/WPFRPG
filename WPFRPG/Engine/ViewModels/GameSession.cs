9af4d41c029deb70105c0be78773d3d2e9c396ed
BrettG <brett@kalain.co.uk>
2019-09-03 13:41:39 +0100

    Player display
    Added the core player data to display in the console.

--- WPFRPG/Engine/ViewModels/GameSession.cs
+++ WPFRPG/Engine/ViewModels/GameSession.cs
@@ -7,15 +7,19 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Gankdalf the Musician";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 100000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
