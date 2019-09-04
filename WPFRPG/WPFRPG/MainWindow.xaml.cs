96544210cfc149bade172b87c41848f6b94602b5
BrettG <brett@kalain.co.uk>
2019-09-04 15:48:16 +0100

    Player Connections
    Linked theplayer to the display

--- WPFRPG/WPFRPG/MainWindow.xaml.cs
+++ WPFRPG/WPFRPG/MainWindow.xaml.cs
@@ -29,5 +29,10 @@ namespace WPFRPG
             _gameSession = new GameSession();
             DataContext = _gameSession;
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
     }
 }
